63c109fbbe0c341988b5aa6a15f05a26ead88176
Charles Daniels <charles@cdaniels.net>
2020-07-29 18:31:12 -0400

    implement mlpx diff sub-command

--- mlpx/cmd/mlpx/main.go
+++ mlpx/cmd/mlpx/main.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"os"
 	"strconv"
+	"time"
 
 	"github.com/akamensky/argparse"
 
@@ -28,6 +29,11 @@ func main() {
 
 	versionFlag := parser.Flag("v", "version", &argparse.Options{Help: "Display version number and exit."})
 
+	randomSeed := parser.String("S", "seed", &argparse.Options{
+		Help:    "Specify a random seed as an integer, or '-' for the current system time",
+		Default: "-",
+	})
+
 	//// new command //////////////////////////////////////////////////////
 
 	newCommand := parser.NewCommand("new", "Generate a new MLPX with random weights and biases.")
@@ -37,10 +43,6 @@ func main() {
 			Help:    "Specify a list of integer layer sizes, in neurons",
 			Default: []int{5, 10, 5},
 			Validate: func(args []string) error {
-				if len(args) < 2 {
-					return fmt.Errorf("Must define at least two layers (input and output)")
-				}
-
 				for i, v := range args {
 					s, err := strconv.Atoi(v)
 					if err != nil {
@@ -77,24 +79,12 @@ func main() {
 		&argparse.Options{
 			Help:    "Specify the upper and lower bounds for the random bias values.",
 			Default: []float64{0, 1.0},
-			Validate: func(args []string) error {
-				if len(args) != 2 {
-					return fmt.Errorf("Must specify a min and max bias value, got %d values instead", len(args))
-				}
-				return nil
-			},
 		})
 
 	newWeightRange := newCommand.FloatList("w", "weight_range",
 		&argparse.Options{
 			Help:    "Specify the upper and lower bounds for the random weight values.",
 			Default: []float64{0, 1.0},
-			Validate: func(args []string) error {
-				if len(args) != 2 {
-					return fmt.Errorf("Must specify a min and max weight value, got %d values instead", len(args))
-				}
-				return nil
-			},
 		})
 
 	newAlpha := newCommand.Float("p", "alpha",
@@ -119,6 +109,28 @@ func main() {
 			Default: "-",
 		})
 
+	//// diff command /////////////////////////////////////////////////////
+
+	diffCommand := parser.NewCommand("diff", "Compare two existing MLPX files")
+
+	diffInput := diffCommand.StringList("i", "input",
+		&argparse.Options{
+			Help:     "Specify two MLPX files to diff.",
+			Required: true,
+		})
+
+	diffIndent := diffCommand.String("I", "indent",
+		&argparse.Options{
+			Help:    "Specify the indent that should be used to show hierarchy.",
+			Default: "\t",
+		})
+
+	diffEpsilon := diffCommand.Float("e", "epsilon",
+		&argparse.Options{
+			Help:    "Epsilon value used when comparing floating point numbers.",
+			Default: 0.00001,
+		})
+
 	err := parser.Parse(os.Args)
 	if err != nil {
 		fmt.Fprint(os.Stderr, parser.Usage(err))
@@ -128,6 +140,17 @@ func main() {
 	if *versionFlag {
 		fmt.Printf("mlpx v0.0.1-git\n")
 		os.Exit(0)
+	}
+
+	if *randomSeed == "-" {
+		rand.Seed(time.Now().UTC().UnixNano())
+	} else {
+		s, err := strconv.Atoi(*randomSeed)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Invalid random seed '%s': %v\n", *randomSeed, err)
+			os.Exit(1)
+		}
+		rand.Seed(int64(s))
 	}
 
 	if newCommand.Happened() {
@@ -142,6 +165,22 @@ func main() {
 
 		if len(*newLayerSizes) != len(activations) {
 			fmt.Fprintf(os.Stderr, "Must specify same number of layer sizes and activation functions")
+			os.Exit(1)
+		}
+
+		if len(*newLayerSizes) < 2 {
+			fmt.Fprintf(os.Stderr, "Must specify at least two layers")
+			os.Exit(1)
+		}
+
+		if len(*newBiasRange) != 2 {
+			fmt.Fprintf(os.Stderr, "Must specify an upper and lower bias range")
+			os.Exit(1)
+		}
+
+		if len(*newWeightRange) != 2 {
+			fmt.Fprintf(os.Stderr, "Must specify an upper and lower weight range")
+			os.Exit(1)
 		}
 
 		m := mlpx.MakeMLPX()
@@ -152,6 +191,11 @@ func main() {
 		snap := m.Snapshots[snapid]
 
 		for index, neurons := range *newLayerSizes {
+			if neurons < 1 {
+				fmt.Fprintf(os.Stderr, "Layer %d must have at least one neuron, requested %d", index, neurons)
+				os.Exit(1)
+			}
+
 			layerID := fmt.Sprintf("%d", index)
 			pred := fmt.Sprintf("%d", index-1)
 			succ := fmt.Sprintf("%d", index+1)
@@ -208,6 +252,7 @@ func main() {
 				os.Exit(1)
 			}
 		}
+
 	} else if validateCommand.Happened() {
 		data := []byte{}
 		if *validateInput == "-" {
@@ -243,6 +288,30 @@ func main() {
 
 		os.Exit(0)
 
+	} else if diffCommand.Happened() {
+		if len(*diffInput) != 2 {
+			fmt.Fprintf(os.Stderr, "Must specify exactly two files to diff")
+			os.Exit(1)
+		}
+
+		m1, err := mlpx.ReadJSON((*diffInput)[0])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to read MLPX file '%s': %v", (*diffInput)[0], err)
+			os.Exit(1)
+		}
+
+		m2, err := mlpx.ReadJSON((*diffInput)[1])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to read MLPX file '%s': %v", (*diffInput)[1], err)
+			os.Exit(1)
+		}
+
+		diffs := m1.Diff(m2, *diffIndent, *diffEpsilon)
+		for _, d := range diffs {
+			fmt.Println(d)
+		}
+
+		os.Exit(len(diffs))
 	}
 
 }
